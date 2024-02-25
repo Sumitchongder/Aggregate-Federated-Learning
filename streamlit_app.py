2b7e49215ea1fbaee59372bc72d171b27ad38f75
Sumit Tapas Chongder <77958774+Sumitchongder@users.noreply.github.com>
2024-02-25 18:09:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import time
 from sklearn.datasets import fetch_openml
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.externals import joblib
+import joblib
 import matplotlib.pyplot as plt
 
 # Load MNIST dataset
